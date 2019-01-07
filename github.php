5760caa204276aaf0af3fa46080c1d894d529b8b
google-test-server <test-server@google.cloud>
2019-01-07 09:32:32 +0000

    changed github.php

--- github.php
+++ github.php
@@ -1,1 +1,9 @@
-<?php 'git pull';
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+shell_exec( ‘cd /var/www/highlight/git-repo/ && git reset –hard HEAD && git pull’ );
+}
+
+?>hi
